d5eb47497f849bc538282b9ad4aaaf8282db2247
ErikCedano <erik_cedano@hotmail.com>
2022-06-09 19:57:03 -0500

    primer commit

---
+++ src/main/java/com/anahuac/calidad/tareasErik/App.java
@@ -0,0 +1,13 @@
+package com.anahuac.calidad.tareasErik;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
