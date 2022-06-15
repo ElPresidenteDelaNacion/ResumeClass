e9e96353b02b7d57f902c33a150fbc0eb8f57823
ErikCedano <erik_cedano@hotmail.com>
2022-06-14 22:50:57 -0500

    actualizacion

--- src/main/java/com/anahuac/calidad/tareasErik/Prueba.java
+++ src/main/java/com/anahuac/calidad/tareasErik/Prueba.java
@@ -1,5 +1,29 @@
 package com.anahuac.calidad.tareasErik;
 
 public class Prueba {
-
+	private float ultimoResultado;
+	
+	public float suma(float primerSumando, float segundoSumando) {
+		return ultimoResultado = primerSumando + segundoSumando;
+	}
+	
+	public float resta(float minuendo, float sustraendo) {
+		return ultimoResultado = minuendo - sustraendo;
+	}
+	
+	public float multiplicacion(float primerFactor, float segundoFactor) {
+		return ultimoResultado = primerFactor*segundoFactor;
+	}
+	
+	public float division(float dividendo, float divisor) throws ArithmeticException{
+		if(divisor ==0)
+			throw new ArithmeticException("La division entre cero no esta permitida");
+		else
+			
+		return ultimoResultado = dividendo/divisor;
+	}
+	
+	public float getUltimaResultado() {
+		return ultimoResultado;
+	}
 }
